94b4ff35a6208673203b3b2f0ff5d84992285b1f
Alexander Davidson Bryan <alxdb@pm.me>
2021-02-07 16:02:38 +0000

    improved visualise.py

--- mazes/visualise.py
+++ mazes/visualise.py
@@ -5,30 +5,47 @@ import cairo
 from mazes import Maze, Coord
 
 
+BACKGROUND = cairo.SolidPattern(0.15, 0.15, 0.15)
+FOREGROUND = cairo.SolidPattern(0.8, 0.8, 0.8)
+
+START = cairo.SolidPattern(0.8, 0.2, 0.2)
+END = cairo.SolidPattern(0.2, 0.8, 0.2)
+PATH = cairo.SolidPattern(0.2, 0.2, 0.8)
+
+
 def draw_maze(
     filename: str,
     maze: Maze,
-    start: Coord,
-    end: Coord,
+    solution: list[Coord],
     scale: int = 10,
     margin: int = 10,
     line_width: float = 0.1,
-):
+) -> None:
+    start = solution[0]
+    end = solution[-1]
+
     image_size = (maze.size * scale) + (2 * margin)
     surface = cairo.SVGSurface(filename, image_size, image_size)
     ctx = cairo.Context(surface)
 
+    # background
+    ctx.set_source(BACKGROUND)
+    ctx.rectangle(0, 0, image_size, image_size)
+    ctx.fill()
+
     # prepare for drawing
     ctx.translate(margin, margin)
     ctx.scale(scale, scale)
-    ctx.set_source_rgb(0.0, 0.0, 0.0)
-    ctx.set_line_width(line_width)
-    ctx.set_line_cap(cairo.LINE_CAP_SQUARE)
 
     # checkerboard iteration pattern
+    ctx.set_source(FOREGROUND)
+    ctx.set_line_width(line_width)
+    ctx.set_line_cap(cairo.LINE_CAP_SQUARE)
     for y in range(maze.size):
         for x in range(0, maze.size, 2):
             x = x + (y % 2)
+            if x >= maze.size:
+                continue
             # map neighbours to edge lines
             for neighbour in maze.neighbours((x, y)):
                 if neighbour not in maze.paths[(x, y)]:
@@ -47,6 +64,7 @@ def draw_maze(
                 ctx.stroke()
 
     # draw borders
+    ctx.move_to(start[0] + 0.5, start[1] + 0.5)
     ctx.move_to(0, 0)
     ctx.line_to(maze.size, 0)
     ctx.line_to(maze.size, maze.size)
@@ -54,13 +72,20 @@ def draw_maze(
     ctx.line_to(0, 0)
     ctx.stroke()
 
-    # draw start and end points
-    ctx.set_source_rgb(1.0, 0.0, 0.0)
-    ctx.arc(start[0] + 0.5, start[1] + 0.5, 0.4, 0.0, math.pi * 2)
+    # solution
+    ctx.set_source(PATH)
+    ctx.move_to(start[0] + 0.5, start[1] + 0.5)
+    for step in solution[1:]:
+        ctx.line_to(step[0] + 0.5, step[1] + 0.5)
     ctx.stroke()
 
-    ctx.set_source_rgb(0.0, 0.0, 1.0)
+    # draw start and end points
+    ctx.set_source(START)
+    ctx.arc(start[0] + 0.5, start[1] + 0.5, 0.4, 0.0, math.pi * 2)
+    ctx.fill()
+
+    ctx.set_source(END)
     ctx.arc(end[0] + 0.5, end[1] + 0.5, 0.4, 0.0, math.pi * 2)
-    ctx.stroke()
+    ctx.fill()
 
     surface.finish()
